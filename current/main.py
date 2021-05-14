c8690f2af3a0a814eac259a9c688a310a251836c
Daksh <dakshkanotra@gmail.com>
2021-05-14 20:46:06 +0530

    Not much changes, just adjusted it to PEP standards.

--- current/main.py
+++ current/main.py
@@ -3,14 +3,14 @@ import discord
 import requests
 import json
 
-# API base URL
 BASE_URL = "https://zenquotes.io/api/random"
 
-class MyClient(discord.Client):
 
+class MyClient(discord.Client):
     async def on_ready(self):
-        #print('Logged on as', self.user)
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="opportunities to pong you"))
+        # print('Logged on as', self.user)
+        await client.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.watching, name="opportunities to pong you"))
 
     async def on_message(self, message):
 
@@ -23,28 +23,30 @@ class MyClient(discord.Client):
         channel = message.channel
 
         # prefix check
-        if(msg.startswith(';p')):
+        if msg.startswith(';p'):
 
             # send code if someone wants it ✨
-            if(msg == ";p code"):
-                await channel.send("Here's the code that makes me live: <https://github.com/GouravKhunger/pingu/blob/main/current/main.py>")
+            if msg == ";p code":
+                await channel.send("Here's the code that makes me live: "
+                                   "<https://github.com/GouravKhunger/pingu/blob/main/current/main.py>")
                 return
 
-            # get a random quote from zenquote's api
-            if(msg == ";p quote"):
+            # get a random quote from zen quote's api
+            if msg == ";p quote":
                 response = requests.get(BASE_URL)
                 json = response.json()
                 quote = json[0]['q']
                 q_author = json[0]['a']
                 await channel.send(
                     quote
-                    +"\n"+
-                    "- "+q_author
+                    + "\n" +
+                    "- " + q_author
                 )
 
         # contest notifications from practice bot
         elif str(channel.id) == "804973113246220309" and str(message.author) == "Practice#2886":
             await channel.send("<@&807807887375990876> ^_^")
+
 
 client = MyClient()
 client.run('<bot-token>')
